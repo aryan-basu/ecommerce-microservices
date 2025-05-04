bdcfaca266273f48913df084d4b7029e292c249b
aryan basu <aryanbasu005@gmail.com>
2025-05-04 17:26:01 +0530

    added .env file

--- notification-service/src/main/java/com/ecommerce/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/ecommerce/notification_service/NotificationServiceApplication.java
@@ -1,10 +1,19 @@
 package com.ecommerce.notification_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
+
+	static {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		String appPassword = dotenv.get("GMAIL_APP_PASSWORD");
+		if (appPassword != null) {
+			System.setProperty("GMAIL_APP_PASSWORD", appPassword);
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
